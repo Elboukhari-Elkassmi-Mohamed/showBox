a4f9c9b05e5ee8e941809781c1536244c22888ee
Elboukhari-Elkassmi-Mohamed <m.elboukhari.elkassmi@hotmail.com>
2023-03-12 14:44:26 +0100

    adding icon logo and lunsh screen logo

--- App.js
+++ App.js
@@ -1,38 +1,80 @@
-import * as React from 'react';
+import {useEffect} from 'react';
 import MoviesContainer from './navigator/MoviesContainer';
 import ShowsContainer from './navigator/ShowsContainer';
 import HomeScreen from './screens/Home'
 import WishList from './screens/WishList';
 import Favorites from './screens/Favourite'
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import SplashScreen from 'react-native-splash-screen';
 
 const Drawer = createDrawerNavigator();
 
 function App() {
+
+  useEffect(()=>{
+    SplashScreen.hide();
+  }),[];
+
+ 
   return (
-    <NavigationContainer>
-      <Drawer.Navigator
+   
+      <NavigationContainer>
+        <Drawer.Navigator useLegacyImplementation
+          screenOptions={{
+            drawerStyle: {
+              backgroundColor: '#282A3A',
+            },
+            labelStyle: {
+              fontWeight: 'bold',
+            },
+            drawerInactiveTintColor:'#C69749'
 
-    >
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Shows" component={ShowsContainer} options={{
-          title: 'Series',
-          headerStyle: {backgroundColor: '#282A3A',},
-          headerTintColor: '#C69749',
-        }} />
-        <Drawer.Screen name="Movies" component={MoviesContainer} options={{
-          title: 'Movies',
-          headerStyle: {backgroundColor: '#282A3A',},
-          headerTintColor: '#C69749',
-        }} />
-        <Drawer.Screen name="Favorites" component={Favorites} />
-        <Drawer.Screen name="WishList" component={WishList} />
+          }}>
 
-      </Drawer.Navigator>
-    </NavigationContainer>
+          <Drawer.Screen name="Home" component={HomeScreen} options={{
+            headerStyle: {backgroundColor: '#282A3A',},
+            headerTintColor: '#C69749',
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="layers-sharp" size={size} color={color} />
+            ),
+          }} />
+          <Drawer.Screen name="Movies" component={MoviesContainer} options={{
+            headerStyle: {backgroundColor: '#282A3A',},
+            headerTintColor: '#C69749',
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="ios-film" size={size} color={color} />
+            ),
+          }} />
+          <Drawer.Screen name="Shows" component={ShowsContainer} options={{
+            headerStyle: {backgroundColor: '#282A3A',},
+            headerTintColor: '#C69749',
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="md-albums" size={size} color={color} />
+            ),
+          }} />
+
+          <Drawer.Screen name="Favorites" component={Favorites}  options={{
+            headerStyle: {backgroundColor: '#282A3A',},
+            headerTintColor: '#C69749',
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="star-sharp" size={size} color={color} />
+            ),
+          }}/>
+          <Drawer.Screen name="WishList" component={WishList}  options={{
+            headerStyle: {backgroundColor: '#282A3A',},
+            headerTintColor: '#C69749',
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="ios-cart" size={size} color={color} />
+            ),
+          }} />
+
+        </Drawer.Navigator>
+      </NavigationContainer>
+   
   );
 }
+
 
 export default App;
